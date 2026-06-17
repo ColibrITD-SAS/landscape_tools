319db5b26abe21c79162d0cc83d071fc4515d0d4
Gabriel Legros <gabriel.legros@colibritd.com>
2026-06-17 13:30:57 +0200

    test

--- landscape_tools/barren_plateaus.py
+++ landscape_tools/barren_plateaus.py
@@ -661,12 +661,12 @@ def plot_layerwise_qubits_padding(
         handlelength=4,
     )
     plt.text(
-        0.98,
-        0.98,
+        0.02,
+        0.02,
         param_text,
         transform=plt.gca().transAxes,
-        ha="right",
-        va="top",
+        ha="left",
+        va="bottom",
         fontsize=11,
         bbox=dict(boxstyle="round", facecolor="white", edgecolor="gray", alpha=0.55),
     )
